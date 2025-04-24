d6cd16a5be4d77bedbef28d1ed4553bcc6027fc9
MDuymazz <duymaz.mrt@gmail.com>
2025-04-24 16:37:05 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,20 @@ const m3uLink = "https://raw.githubusercontent.com/MDuymazz/sitem3u/refs/heads/m
 async function handleRequest(request) {
     const url = new URL(request.url);
     const ip = request.headers.get("CF-Connecting-IP");
-    let key = url.searchParams.get("key");
+    const key = url.searchParams.get("key");
 
     if (!key || !ip) {
         return new Response("IP veya key bulunamadı!", { status: 400 });
     }
 
-    // .m3u uzantısını ekle
-    if (!key.endsWith('.m3u')) {
-        key = key + '.m3u';
-    }
-
-    // Kullanıcıları yükle
     const usersResponse = await fetch(usersUrl);
     const usersData = await usersResponse.json();
     const user = usersData[ip];
 
-    if (!user || user.secret_key !== key.slice(0, -4)) { // Sonundaki .m3u'yu çıkartıyoruz
+    if (!user || user.secret_key !== key) {
         return new Response("Geçersiz key veya IP!", { status: 403 });
     }
 
-    // Token'ın geçerlilik süresi dolmadığını kontrol et
     const currentDate = new Date();
     const expireDate = new Date(user.expire_date);
 
@@ -32,13 +25,22 @@ async function handleRequest(request) {
         return new Response("Token süresi dolmuş!", { status: 403 });
     }
 
-    // M3U dosyasını al
     const m3uResponse = await fetch(m3uLink);
     const m3uData = await m3uResponse.text();
 
-    // M3U dosyasını raw formatında geri döndür
-    return new Response(m3uData, {
-        headers: { "Content-Type": "application/x-mpegURL" }
+    const htmlResponse = `
+        <html>
+            <head><title>Playlist</title></head>
+            <body>
+                <pre>${m3uData}</pre>
+            </body>
+        </html>
+    `;
+
+    return new Response(htmlResponse, {
+        headers: {
+            "Content-Type": "text/html"
+        }
     });
 }
 
